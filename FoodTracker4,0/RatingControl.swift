9585ee6216b043adbc25b32c818d8f2e0d6ac0ce
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-10-14 12:50:51 -0400

    Add images to Simulator / Create a subclass of UIView

---
+++ FoodTracker4,0/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  FoodTracker4,0
+//
+//  Created by Rollin Francois on 10/14/16.
+//  Copyright © 2016 Rollin Francois. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+
+}
