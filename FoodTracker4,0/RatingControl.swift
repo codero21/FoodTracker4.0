51b91d3f8b6575461ebbe80089b5027d257de85c
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-10-14 22:04:00 -0400

    Declare a constant for the size of the buttons

--- FoodTracker4,0/RatingControl.swift
+++ FoodTracker4,0/RatingControl.swift
@@ -24,7 +24,7 @@ class RatingControl: UIView {
         
         for _ in 0..<starCount {
             
-            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            let button = UIButton()
             
             button.backgroundColor = UIColor.red
             
@@ -38,21 +38,30 @@ class RatingControl: UIView {
         }
     }
     
-    override public var intrinsicContentSize: CGSize {
-        //...
-        return CGSize(width: 240, height: 44)
-    }
-    
     override func layoutSubviews() {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // Set the button's width and height to a square the size of the frame's height.
+        // let buttonSize = Int(frame.size.height)
+        let buttonSize = 44
+        
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
         // Offset each button's origin by the length of the button plus spacing.
         for(index, button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
             
             button.frame = buttonFrame
         }
         
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        let buttonSize = Int(frame.size.height)
+        
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        //...
+        return CGSize(width: width, height: buttonSize)
     }
 
     // MARK: Button Action
