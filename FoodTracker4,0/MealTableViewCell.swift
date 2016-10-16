3c101b8382aa8cfb30e72db9bb962e1dc0732770
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-10-16 14:58:17 -0400

    Create a subclass of UITableViewCell

---
+++ FoodTracker4,0/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker4,0
+//
+//  Created by Rollin Francois on 10/16/16.
+//  Copyright © 2016 Rollin Francois. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
