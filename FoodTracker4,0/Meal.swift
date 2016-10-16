f5df968f529c925409dfa259fa81207289438dfd
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-10-15 21:11:52 -0400

    Define a data model for a meal

--- FoodTracker4,0/Meal.swift
+++ FoodTracker4,0/Meal.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2016 Rollin Francois. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo:UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+    
+    
+    
+    
+}
+
+
