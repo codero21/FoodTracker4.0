0fe610f51a49005210b558fdffa7868124c27db9
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-10-18 00:50:31 -0400

    Load the initial data

--- FoodTracker4,0/MealvTableViewController.swift
+++ FoodTracker4,0/MealvTableViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class MealvTableViewController: UITableViewController {
-
+    // MARK: Properties
+    var meals = [Meal]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +22,21 @@ class MealvTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    // This is a helper method to load sample data into the app.
+    open func loadSampleMeals() {
+        let photo1 = UIImage(named: "meal1")!
+        let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4)!
+        
+        let photo2 = UIImage(named: "meal2")!
+        let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5)!
+        
+        let photo3 = UIImage(named: "meal3")!
+        let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3)!
+        
+        // meals += [meal1, meal2, meal3]
+        meals += [meal1, meal2, meal3]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
